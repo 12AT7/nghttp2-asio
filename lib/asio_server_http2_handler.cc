4158579f5805d5cd750b921a64ae14dd229ea293
John Galbraith <john.galbraith17@gmail.com>
2023-09-24 17:27:09 -0700

    Resurrect cleanup integration tests

--- lib/asio_server_http2_handler.cc
+++ lib/asio_server_http2_handler.cc
@@ -398,13 +398,11 @@ int http2_handler::submit_trailer(stream &strm, header_map h) {
 
 void http2_handler::enter_callback() {
   assert(!inside_callback_);
-  std::cout << "enter callback" << std::endl;
   inside_callback_ = true;
 }
 
 void http2_handler::leave_callback() {
   assert(inside_callback_);
-  std::cout << "leave callback" << std::endl;
   inside_callback_ = false;
 }
 
